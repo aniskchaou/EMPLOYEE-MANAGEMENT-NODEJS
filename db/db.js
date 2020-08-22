a1fc29a729be79a94f3c1bc9ef1f5bd03378f36a
Anis KCHAOU <kchaouanis25@gmail.com>
2020-08-22 21:49:22 +0200

    Update db.js

--- db/db.js
+++ db/db.js
@@ -1,10 +1,10 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'employee-management',
-    password: 'root',
+    user: 'chbyiyvpixlzez',
+    host: 'ec2-34-236-215-156.compute-1.amazonaws.com',
+    database: 'd44nd3kpidbi5r',
+    password: '5b961ee894b0d83e8254d99a023484edb893aac3ec1d10f1bbdb67d0d5e69713',
     port: 5432,
 });
 
